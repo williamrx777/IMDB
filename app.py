60a290dae026f6a44af2d8370fc556fcbba95a47
william rodrigues <38387964+williamrx777@users.noreply.github.com>
2023-02-07 00:38:05 -0300

    removi 1 html e reduzi a rota e adicionei models

--- app.py
+++ app.py
@@ -1,58 +1,47 @@
 from flask import Flask,render_template,request,redirect,url_for
 import requests
-
+from models.sinopse import Sinopse
+import json
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def index(nome=None):
-    if request.method == 'POST':
-        sinopse = request.form["nome"]
-        return redirect(url_for("sinopse", nome=sinopse))
-    else:
-        nome = 'the batman'
-        default = 'api_key=eeacc6e1e96d273f543d586d8a44bebe'
-        response = requests.get(f'https://api.themoviedb.org/3/search/movie?{default}&query={nome}')
-        dados = response.json()
-        titulo = dados['results'][0]['original_title']
-        sinopse = dados['results'][0]['overview']
-        release_date = dados['results'][0]['release_date']
-        imagem = dados['results'][0]['poster_path']
-        foto = f'https://image.tmdb.org/t/p/w500/{imagem}'
-        return render_template('index.html',titulo=titulo,foto=foto,sinopse=sinopse,nome=nome,release_date=release_date)
+@app.route('/')
+def index():
+    nome = 'the batman'
+    default = 'api_key=eeacc6e1e96d273f543d586d8a44bebe'
+    response = requests.get(f'https://api.themoviedb.org/3/search/movie?{default}&query={nome}')
+    dados = response.json()
+    titulo = dados['results'][0]['title']
+    descricao = dados['results'][0]['overview']
+    release_date = dados['results'][0]['release_date']
+    imagem = dados['results'][0]['poster_path']
+    foto = f'https://image.tmdb.org/t/p/w500/{imagem}'
+    return render_template('index.html',titulo=titulo,foto=foto,descricao=descricao,nome=nome,release_date=release_date)
 
-@app.route('/sinopse/<nome>', methods=['GET','POST'])
-def sinopse(nome=None):
-    if request.method == 'POST':
-        sinopse = request.form["nome"]
-        return redirect(url_for("filme", nome=sinopse))
-    else:
-        nome
+@app.route('/sinopse', methods=['POST'])
+def sinopse():
+    sinopse = Sinopse(request.form['titulo'],['descricao'],['release_date'],['imagem'])
+    try:
         default = 'api_key=eeacc6e1e96d273f543d586d8a44bebe'
-        response = requests.get(f'https://api.themoviedb.org/3/search/movie?{default}&query={nome}')
-        dados = response.json()
-        titulo = dados['results'][0]['original_title']
-        sinopse = dados['results'][0]['overview']
-        release_date = dados['results'][0]['release_date']
-        imagem = dados['results'][0]['poster_path']
+        response = json.loads(requests.get(f'https://api.themoviedb.org/3/search/movie?{default}&query={sinopse.titulo}').text)
+        titulo = response['results'][0]['title']
+        descricao = response['results'][0]['overview']
+        release_date = response['results'][0]['release_date']
+        imagem = response['results'][0]['poster_path']
         foto = f'https://image.tmdb.org/t/p/w500/{imagem}'
-        return render_template('sinopse.html',titulo=titulo,foto=foto,sinopse=sinopse,nome=nome,release_date=release_date)
+        sinopse.titulo=titulo
+        sinopse.descricao=descricao
+        sinopse.release_date=release_date
+        sinopse.imagem=imagem
 
-@app.route('/filme/<nome>', methods=['GET','POST'])
-def filme(nome=None):
-    if request.method == 'POST':
-        sinopse = request.form["nome"]
-        return redirect(url_for("sinopse", nome=sinopse))
-    else:
-        nome
-        default = 'api_key=eeacc6e1e96d273f543d586d8a44bebe'
-        response = requests.get(f'https://api.themoviedb.org/3/search/movie?{default}&query={nome}')
-        dados = response.json()
-        titulo = dados['results'][0]['original_title']
-        sinopse = dados['results'][0]['overview']
-        release_date = dados['results'][0]['release_date']
-        imagem = dados['results'][0]['poster_path']
-        foto = f'https://image.tmdb.org/t/p/w500/{imagem}'
-        return render_template('filme.html',titulo=titulo,foto=foto,sinopse=sinopse,nome=nome,release_date=release_date)
+        return render_template('index.html',
+                            titulo=sinopse.titulo,
+                            imagem=sinopse.imagem,
+                            descricao=sinopse.descricao,
+                            release_date=sinopse.release_date,
+                            foto=foto
+                            )
+    except:
+        'Movie not found'
 
 if __name__=='__main__':
     app.run(debug=True)
---
+++ models/sinopse.py
@@ -0,0 +1,6 @@
+class Sinopse:
+    def __init__(self,titulo,descricao,release_date,imagem):
+        self.titulo=titulo
+        self.descricao=descricao
+        self.release_date=release_date
+        self.imagem=imagem
